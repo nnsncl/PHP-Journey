532ed06746d52a38907a0343770e41bb7de25770
nnsncl <nicolas.mgta@gmail.com>
2021-02-24 12:53:47 +0100

    edit and save files

--- galery/galery.php
+++ galery/galery.php
@@ -12,6 +12,23 @@ list($file_w, $file_h) = getimagesize($_FILES['file']['tmp_name']);
 $image = imagecreatefromjpeg($_FILES['file']['tmp_name']);
 $background = imagecreatetruecolor($background_w, $background_h);
 
+if($file_w > $file_h) {
+    $resized_thumb_w = $background_w;
+    $resized_thumb_h = ceil($file_h * $resized_thumb_w / $file_w);
+
+    $resized_background_w = $background_w;
+    $resized_background_h = ceil($file_h * $background_w / $file_w);
+} else {
+    $resized_thumb_h = $background_h;
+    $resized_thumb_w = ceil($file_w * $resized_thumb_h / $file_h);
+
+    $resized_background_h = $background_h;
+    $resized_background_w = ceil($file_w * $background_h / $file_h);
+}
+
+$pos_x = $background_w / 2 - $resized_thumb_w / 2;
+$pos_y =  $background_h / 2 - $resized_thumb_h / 2;
+
 imagecopyresampled(
     $background,
     $image,
@@ -19,8 +36,8 @@ imagecopyresampled(
     0,
     0,
     0,
-    $background_w,
-    $background_h,
+    $resized_thumb_w,
+    $resized_thumb_h,
     $file_w,
     $file_h
 );

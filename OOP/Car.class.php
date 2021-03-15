3c7085ff9ceb4e23c84aad9d3448c645cb1bed8a
nnsncl <nicolas.mgta@gmail.com>
2021-03-15 10:07:57 +0100

    create a basic class

---
+++ OOP/Car.class.php
@@ -0,0 +1,16 @@
+<?php
+
+class Car
+{
+    // Attributes
+    public $doorAmount = 3;
+    public $brand;
+    public $model;
+    public $color = 'Black';
+
+    // Methods
+    public function start()
+    {
+        echo 'Car starting';
+    }
+}

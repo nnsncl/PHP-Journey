39023259979af79655638ab7513b8270ce51c007
nnsncl <nicolas.mgta@gmail.com>
2021-03-15 10:24:57 +0100

    create methods

--- OOP/Car.class.php
+++ OOP/Car.class.php
@@ -3,18 +3,35 @@
 class Car
 {
     // Attributes
-    public $doorAmount = 3;
-    public $brand;
-    public $model;
-    public $color = 'Black';
+    private $doorAmount = 3;
+    private $brand;
+    private $model;
+    private $color = 'Black';
 
     // Methods
     public function start()
     {
         echo "Car's starting";
     }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
 }
 
 
 $car = new Car;
+
 $car->start();
+echo '<br>';
+echo $car->getColor();
+echo $car->setColor('Grey');
+echo '<br>';
+echo $car->getColor();
+echo '<br>';
